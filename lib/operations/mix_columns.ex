7a3b013de40e0479e215c92c72af2f40021be365
fellipessanha <fellipe900@gmail.com>
2026-05-09 17:32:24 -0300

    improve mix columns

--- lib/operations/mix_columns.ex
+++ lib/operations/mix_columns.ex
@@ -1,7 +1,5 @@
 defmodule Rijndaex.Operations.MixColumns do
-  def add(a, b), do: Bitwise.bxor(a, b)
-
-  @doc """
+  @moduledoc """
   Specific _multiplication_ operation used in this algorithm.
 
   it's a mix of bitwise xors and rightshifts in b operation.
@@ -18,6 +16,7 @@ defmodule Rijndaex.Operations.MixColumns do
     iex> Rijndaex.Operations.MixColumns.revert([159, 220, 88, 157])
     [242, 10, 34, 92]
   """
+  import Bitwise, only: [&&&: 2, <<<: 2, >>>: 2, bxor: 2]
 
   @inverse_matrix [
     [14, 11, 13, 9],
@@ -26,26 +25,28 @@ defmodule Rijndaex.Operations.MixColumns do
     [11, 13, 9, 14]
   ]
 
+  defp add(a, b), do: bxor(a, b)
+
   def mul(a, b), do: mul(a, b, 0)
   defp mul(_, 0, result), do: result
 
   defp mul(a, b, acc) do
-    acc = if Bitwise.&&&(b, 1) != 0, do: Bitwise.bxor(acc, a), else: acc
-    mul(mul2(a), Bitwise.>>>(b, 1), acc)
+    acc = if (b &&& 1) != 0, do: add(acc, a), else: acc
+    mul(mul2(a), b >>> 1, acc)
   end
 
-  defp mul2(a) when Bitwise.&&&(a, 0x80) != 0,
-    do: Bitwise.<<<(a, 1) |> Bitwise.bxor(0x1B) |> rem(0x100)
+  defp mul2(a) when (a &&& 0x80) != 0,
+    do: (a <<< 1) |> add(0x1B) |> rem(0x100)
 
-  defp mul2(a), do: Bitwise.<<<(a, 1) |> rem(0x100)
+  defp mul2(a), do: (a <<< 1) |> rem(0x100)
 
   def apply(column) do
-    xored = Enum.reduce(column, &Bitwise.bxor/2)
+    xored = Enum.reduce(column, &add/2)
 
     column
     |> Enum.chunk_every(2, 1, column)
     |> Enum.map(fn [a, b] ->
-      mul2(Bitwise.bxor(a, b)) |> Bitwise.bxor(xored) |> Bitwise.bxor(a)
+      mul2(add(a, b)) |> add(xored) |> add(a)
     end)
   end
 
@@ -53,7 +54,7 @@ defmodule Rijndaex.Operations.MixColumns do
     for row <- @inverse_matrix do
       Enum.zip(column, row)
       |> Enum.map(fn {a, b} -> mul(a, b) end)
-      |> Enum.reduce(&Bitwise.bxor/2)
+      |> Enum.reduce(&add/2)
     end
   end
 end
