6adde5b69c81eda262d911f3967d68773ff30965
fellipessanha <fellipe900@gmail.com>
2026-05-31 22:15:26 -0300

    remove old unused code

--- lib/rijndaex.ex
+++ lib/rijndaex.ex
@@ -19,10 +19,6 @@ defmodule Rijndaex do
 
   """
 
-  alias Operations.{MixColumns, SubBytes, ShiftRow}
-  import Operations, only: [add: 2]
-  import Utils, only: [as_matrix: 1]
-
   @type cypher_strategies() :: :naive | :ecb | :cbc
 
   @spec cypher_blocks(binary(), binary(), cypher_strategies, keyword) :: binary()
@@ -31,39 +27,9 @@ defmodule Rijndaex do
   def cypher_blocks(key, input, :ecb, _), do: Strategies.ECB.cypher_blocks(key, input)
   def cypher_blocks(key, input, :cbc, _), do: Strategies.CBC.cypher_blocks(key, input)
 
-  @doc """
-  Encrypts a single 4×4 matrix block using the expanded key schedule.
-
-  `block` must already be in matrix form (list of 4 four-byte lists), as
-  returned by the private `as_matrix/1` helper. `key` is the original cipher
-  key (binary or list) used for the initial AddRoundKey step.
-  """
-  def cypher_block(block, expanded_key, key) do
-    round_key = key |> as_matrix()
-
-    add(block, round_key)
-    |> apply_rounds(expanded_key)
-  end
-
-  defp apply_rounds(block, [round_key]) do
-    block
-    |> SubBytes.apply()
-    |> ShiftRow.apply()
-    |> add(round_key)
-  end
-
-  defp apply_rounds(block, [round_key | other_keys]) do
-    block
-    |> SubBytes.apply()
-    |> ShiftRow.apply()
-    |> MixColumns.apply()
-    |> add(round_key)
-    |> apply_rounds(other_keys)
-  end
-
   @spec uncypher_blocks(binary(), binary(), cypher_strategies) :: binary()
   def uncypher_blocks(key, input, strategy \\ :naive)
   def uncypher_blocks(key, input, :naive), do: Strategies.Naive.uncypher_blocks(key, input)
   def uncypher_blocks(key, input, :ecb), do: Strategies.ECB.uncypher_blocks(key, input)
-  def uncypher_blocks(key, input, :cbc), do: Strategies.CBC.uncypher_blocks(key, input)
+  def uncypher_blocks(key, input, :cbc, _), do: Strategies.CBC.cypher_blocks(key, input)
 end
